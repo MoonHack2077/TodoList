2d8b91aee27eeadf355bf4f163fef258b0c33073
JuanManuelArenasRincon20077 <arenasrinconj@gmail.com>
2022-03-01 22:48:33 -0500

    deleted files not necessary and create directory with my components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { TodoCounter } from './components/TodoCounter.js'
+import React from 'react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <TodoCounter/>
+      {/* <TodoSearch/> */}
+      <input placeholder='Add a new todo...' />
+      {/* <TodoList>
+        <TodoItem/>
+      </TodoList>
+      <CreateTodoButton/> */}
+      <button>+</button>
+    </React.Fragment>
   );
 }
 
---
+++ src/components/TodoCounter.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function TodoCounter(){
+    return(
+      <h2>I have done 2 of 3 todos</h2>
+    )
+}
+
+export { TodoCounter }
