2665a610a7b8446c3d23a18005cf60d7d7c9155b
JuanManuelArenasRincon20077 <arenasrinconj@gmail.com>
2022-03-05 18:07:20 -0500

    whaaaaat?, i gonna try with checkboxes

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -5,9 +5,19 @@ import { faTrash, faCheck } from '@fortawesome/free-solid-svg-icons'
 
 
 function TodoItem(props){
-    const check = (e) => {
+    const check = e => {
         console.log(e.target);
-        e.target.classList.toggle('icon-check');
+        console.log(e.target.parentElement);
+        const target = e.target;
+        const parent = target.parentElement
+
+        if(target.classList.contains('icon-check')){
+            target.classList.remove('icon-check');
+            parent.classList.remove('icon-check');
+        }else{
+            target.classList.remove('icon-check');
+            parent.classList.remove('icon-check');
+        }
         // document.getElementById(`${e.target.parentElement.id}`).classList.toggle('icon-check');
     };
     const remove = () => console.log('remove .-.');
@@ -20,7 +30,7 @@ function TodoItem(props){
             <div>
                 <p className='text' >{props.text}</p>
             </div>
-            <div className='icontainer' onClick={e=>check(e)}>
+            <div className='icontainer' onClick={check}>
                 <FontAwesomeIcon icon={faCheck} className={`icon check`}/>
             </div>
         </li>
