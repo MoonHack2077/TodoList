39bc9d17a8a502689a00f37c8fdeba6b311f40e6
JuanManuelArenasRincon20077 <arenasrinconj@gmail.com>
2022-03-22 11:49:14 -0500

    Improve responsive filters, very well

--- src/components/Create/Button/CreateTodoButton.jsx
+++ src/components/Create/Button/CreateTodoButton.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import './CreateButton.css';
 
-function CreateTodoButton({onClick}){
+function CreateTodoButton({ onClick }){
     return (
-      <div className='CreateTodoButton' onClick={onClick} >
+      <div className='CreateTodoButton' onClick={ onClick } >
         <input type='button' value='Add' className='add' />
       </div>
     )
--- src/components/Create/Detail/Detail.jsx
+++ src/components/Create/Detail/Detail.jsx
@@ -5,8 +5,13 @@ import './Detail.css';
 function Detail({ info, placeholder,value, onChange }){
     return(
         <div className='Detail'>
-            <label className='Detail-label' htmlFor={info}>{info}</label>
-            <TodoSearch className='TodoSearch' value={value} onChange={onChange} placeholder={placeholder} info={info}/>
+            <label className='Detail-label' htmlFor={ info }>{ info }</label>
+            <TodoSearch 
+                className='TodoSearch' 
+                value={ value } 
+                onChange={ onChange } 
+                placeholder={ placeholder } 
+                info={ info }/>
         </div>
     )
 }
