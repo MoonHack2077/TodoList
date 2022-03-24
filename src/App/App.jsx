8bfecf92417ce39fe33bb9086161079fbfc45d30
JuanManuelArenasRincon20077 <arenasrinconj@gmail.com>
2022-03-24 12:05:16 -0500

    Try to separete logic of the main App

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -4,136 +4,14 @@ import { NavBar } from '../components/NavBar/Nav/NavBar.jsx';
 import { Detail } from '../components/Create/Detail/Detail.jsx';
 import { Modal } from '../components/Modal/Modal.jsx';
 import { Alert } from '../components/Alert/Alert.jsx';
-import { useState, useEffect } from 'react';
-import { v4 as uuid } from 'uuid';
+import { AppLogic } from '../Logic/AppLogic.js';
 import './App.css';
 
-const KEY = 'TODOS';
 
 function App() {
-  // States
-  const [ todos , setTodos ] = useState( JSON.parse( localStorage.getItem( KEY ) ) );
-  const [ title , setTitle ] = useState('');
-  const [ description , setDescription ] = useState('');
-  const [ search , setSearch ] = useState('');
-  const [ hideModal , setHideModal ] = useState(true);
-  const [ hideAlertDetail , setHideAlertDetail ] = useState(true);
-  const [ hideAlertModal , setHideAlertModal ] = useState(true);
-  const [ newTitle , setNewTitle ] = useState('');
-  const [ newDescription , setNewDescription ] = useState('');
-  const [ completed , setCompleted ] = useState(false);
-  const [ TODO , SETTODO ]= useState({});
-
-  // useEffect to look out to all todos and set these at the localStorage
-  useEffect( () => localStorage.setItem( KEY , JSON.stringify( todos ) ) , [todos]);
-
-  //Create todo
-  const create = () => {
-    if(!title || !description){
-      setHideAlertDetail(false);
-      return;
-    }
-    setHideAlertDetail(true);
-    const todo = { title , description , done: false , id: uuid() };
-    setTodos([...todos, todo]);
-    setTitle('');
-    setDescription('');
-  }
-
-  //Find index of nesessary todo
-  const findTodo = id => todos.findIndex( todo => todo.id === id );
-
-  //Set up todos
-  const setUpTodo = targetId => {
-    const newTodos = [...todos];
-    const index = findTodo(targetId);
-    let { title , description , done , hide , id } = newTodos[index];
-
-    const toggleCheck = () => {
-      newTodos[index].done = !newTodos[index].done;
-      setTodos(newTodos);
-    }
-
-    const remove = () => {
-      newTodos.splice( index , 1 );
-      setTodos(newTodos);
-    };
-
-    const edit = () => {
-      setNewTitle(title);
-      setNewDescription(description);
-      setCompleted(done);
-      SETTODO({ title, description, done, hide, id });
-      setHideModal(!hideModal);
-    };
-  
-    return { remove , edit , toggleCheck  };
-  }
-
-  //Modal settings
-  const modalSettings = () => {
-    const index = findTodo(TODO.id);
-    const newTodos = [...todos];
-
-    const toggleCompleted = () => {
-      TODO.done = !TODO.done;
-      setCompleted(TODO.done);
-      newTodos[index].done = completed;
-      setTodos(newTodos);
-    }
-  
-    const setNewValues = () => {
-      if(!newTitle || !newDescription){
-        setHideAlertModal(false);
-        return;
-      }
-      setHideAlertModal(true);
-      //Injecting new values
-      TODO.title = newTitle;
-      TODO.description = newDescription;
-      TODO.done = completed;
-      TODO.hide = false;
-      TODO.id = uuid();
-
-      SETTODO({...TODO});
-
-      newTodos.splice( index, 1 , TODO );
-
-      setTodos(newTodos);
-      setHideModal(!hideModal);
-    }
-
-    return { toggleCompleted , setNewValues }
-  }
-
-  //Filter searched todos
-  const filters = target => {
-    const { id , checked } = target;
-
-    for(const todo of todos){
-      todo.hide=false;
-      const searchedLower = search.toLowerCase();
-      const hasSearched = todo.title.toLowerCase().includes(searchedLower) || todo.description.toLowerCase().includes(searchedLower);
-
-      if( (hasSearched) && (id==='Done' && checked && todo.done)){
-        todo.hide = false;
-      }else if( (hasSearched) && (id==='Uncompleted' && checked && !todo.done)){
-        todo.hide = false;
-      }else if( (hasSearched) && (id==='All' && checked)){
-        todo.hide = false;
-      }else{
-        todo.hide = true;
-      }
-      setTodos([...todos]);
-    }
-
-  }
-
-  const removeAllDone = () => {
-    const newTodos = [...todos];
-    setTodos( newTodos.filter( todo => !todo.done ) );
-  }
-
+  const { create , setUpTodo , modalSettings , filters , removeAllDone , getHideAlerts , SetSearch , States } = AppLogic();
+  const { hideAlertDetail , hideAlertModal } = getHideAlerts();
+  const { todos , title , description , search , hideModal , newTitle , newDescription , completed } = States();
   return (
     <div className = 'App'>
 
@@ -150,7 +28,7 @@ function App() {
 
       <NavBar 
         value={ search } 
-        onChange={ e => setSearch( e.target.value ) }
+        onChange={ e => SetSearch( e.target.value ) }
         onClick = { e => filters( e ) }
       />
       
---
+++ src/App/Logic/AppLogic.js
@@ -0,0 +1,147 @@
+import { useState, useEffect } from 'react';
+import { v4 as uuid } from 'uuid';
+
+const KEY = 'TODOS';
+
+function AppLogic(){
+    // States
+    const [ todos , setTodos ] = useState( JSON.parse( localStorage.getItem( KEY ) ) );
+    const [ title , setTitle ] = useState('');
+    const [ description , setDescription ] = useState('');
+    const [ search , setSearch ] = useState('');
+    const [ hideModal , setHideModal ] = useState(true);
+    const [ hideAlertDetail , setHideAlertDetail ] = useState(true);
+    const [ hideAlertModal , setHideAlertModal ] = useState(true);
+    const [ newTitle , setNewTitle ] = useState('');
+    const [ newDescription , setNewDescription ] = useState('');
+    const [ completed , setCompleted ] = useState(false);
+    const [ TODO , SETTODO ]= useState({});
+
+    const getHideAlerts = () =>{
+        return { hideAlertDetail , hideAlertModal }
+    }
+
+    const SetStates = param =>{
+        return { setTitle(param) , setDescription(param) , setNewTitle(param) , setNewDescription(param) }
+    }
+    
+    const States = () =>{
+        return { todos , title , description , search , hideModal , newTitle , newDescription , completed , TODO }
+    }
+    // useEffect to look out to all todos and set these at the localStorage
+    useEffect( () => localStorage.setItem( KEY , JSON.stringify( todos ) ) , [todos]);
+
+    //Create todo
+    const create = () => {
+        if(!title || !description){
+        setHideAlertDetail(false);
+        return;
+        }
+        setHideAlertDetail(true);
+        const todo = { title , description , done: false , id: uuid() };
+        setTodos([...todos, todo]);
+        setTitle('');
+        setDescription('');
+    }
+
+
+    //Find index of nesessary todo
+    const findTodo = id => todos.findIndex( todo => todo.id === id );
+
+    //Set up todos
+    const setUpTodo = targetId => {
+        const newTodos = [...todos];
+        const index = findTodo(targetId);
+        let { title , description , done , hide , id } = newTodos[index];
+
+        const toggleCheck = () => {
+        newTodos[index].done = !newTodos[index].done;
+        setTodos(newTodos);
+        }
+
+        const remove = () => {
+        newTodos.splice( index , 1 );
+        setTodos(newTodos);
+        };
+
+        const edit = () => {
+        setNewTitle(title);
+        setNewDescription(description);
+        setCompleted(done);
+        SETTODO({ title, description, done, hide, id });
+        setHideModal(!hideModal);
+        };
+    
+        return { remove , edit , toggleCheck  };
+    }
+
+    //Modal settings
+    const modalSettings = () => {
+        const index = findTodo(TODO.id);
+        const newTodos = [...todos];
+
+        const toggleCompleted = () => {
+        TODO.done = !TODO.done;
+        setCompleted(TODO.done);
+        newTodos[index].done = completed;
+        setTodos(newTodos);
+        }
+    
+        const setNewValues = () => {
+        if(!newTitle || !newDescription){
+            setHideAlertModal(false);
+            return;
+        }
+        setHideAlertModal(true);
+        //Injecting new values
+        TODO.title = newTitle;
+        TODO.description = newDescription;
+        TODO.done = completed;
+        TODO.hide = false;
+        TODO.id = uuid();
+
+        SETTODO({...TODO});
+
+        newTodos.splice( index, 1 , TODO );
+
+        setTodos(newTodos);
+        setHideModal(!hideModal);
+        }
+
+        return { toggleCompleted , setNewValues }
+    }
+
+    //Filter searched todos
+    const filters = target => {
+        const { id , checked } = target;
+
+        for(const todo of todos){
+        todo.hide=false;
+        const searchedLower = search.toLowerCase();
+        const hasSearched = todo.title.toLowerCase().includes(searchedLower) || todo.description.toLowerCase().includes(searchedLower);
+
+        if( (hasSearched) && (id==='Done' && checked && todo.done)){
+            todo.hide = false;
+        }else if( (hasSearched) && (id==='Uncompleted' && checked && !todo.done)){
+            todo.hide = false;
+        }else if( (hasSearched) && (id==='All' && checked)){
+            todo.hide = false;
+        }else{
+            todo.hide = true;
+        }
+            setTodos([...todos]);
+        }
+
+    }
+
+    
+    //Function to remove all todos done
+    const removeAllDone = () => {
+        const newTodos = [...todos];
+        setTodos( newTodos.filter( todo => !todo.done ) );
+    }
+
+    return { create , setUpTodo , modalSettings , filters , removeAllDone , getHideAlerts , SetStates , States}
+}
+
+export { AppLogic }
